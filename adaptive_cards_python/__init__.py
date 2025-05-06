17ceef7d35b875cce8301098ed8d4346eb4bad00
Emir Karamehmetoglu <emirk@axis.com>
2025-05-06 14:47:30 +0200

    Add dynamic version to __init__
    Change-Id: Ife8234e4bdb156ca61762edd24c909cb1a08ce16

--- adaptive_cards_python/__init__.py
+++ adaptive_cards_python/__init__.py
@@ -1,6 +1,14 @@
+import importlib.metadata
+
 from .adaptive_card import AdaptiveCard, Element as elements, Action as actions
 from .validation import validate_using_json_schema
 from .msteams import post_to_webhook, create_payload, Payload
+
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.1.0"  # Fallback for development mode
 
 __all__ = [
     "AdaptiveCard",
@@ -10,4 +18,5 @@ __all__ = [
     "post_to_webhook",
     "create_payload",
     "Payload",
+    "__version__"
 ]
